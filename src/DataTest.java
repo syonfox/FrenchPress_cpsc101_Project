0d121ea394e5e67f71389204c28f860afe26e2b9
SyonFox <kier.lightning@gmail.com>
2016-02-28 08:09:46 -0800

    Added Data and Data test whitch can take in a csv file and parse it.

---
+++ src/DataTest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/** Main takes no arguments but must have file names set inside of it **/
+public class DataTest {
+  
+  public static void main(String[] args) {
+  
+    String inputFile = "testData.csv";
+    Data lines = new Data(inputFile);
+    lines.loadData();
+    lines.printFile();
+    
+   
+  }
+}
