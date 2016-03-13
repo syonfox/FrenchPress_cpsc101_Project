e8750ba7f0c2c1c66bc9e0ee385a1b74e4ac2ba6
RodrigoNicoSantoro <rodrigo.nico.santoro@gmail.com>
2016-03-13 13:45:49 -0700

    add/remove button working

---
+++ src/CourseManager.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class CourseManager {
+	
+	ArrayList<Course> courses;
+	
+	public ArrayList<Course> toCourseArrayList(ArrayList<String> s){
+		courses = new ArrayList<Course>();
+		
+		
+		
+		return courses;
+	}
+}
--- src/GUI.java
+++ src/GUI.java
@@ -6,10 +6,12 @@
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -30,8 +32,13 @@ import java.awt.Dimension;
 
 public class GUI {
 
-	public static JFrame frame;
-
+	private static JFrame frame;
+	private static CourseManager cm;
+	private static String[] arrCourses = {""};
+	private static JList<String> courseList;
+	private static JList<String> timeTableList;
+	private static DefaultListModel<String> timeTableListModel = new DefaultListModel<String>();
+	
 	private static final int WIDTH = 400;
 	private static final int HEIGHT = 400;
 	private static TimeTablePanel ttp;
@@ -49,28 +56,32 @@ public class GUI {
             // Set cross-platform Java L&F (also called "Metal")
         UIManager.setLookAndFeel(
             "com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
-    }
-    catch (UnsupportedLookAndFeelException e) {
-       // handle exception
-			 //System.out.println(e.toString());
-    }
-    catch (ClassNotFoundException e) {
-       // handle exception
-			 //System.out.println(e.toString());
-    }
-    catch (InstantiationException e) {
-       // handle exception
-			 //System.out.println(e.toString());
-    }
-    catch (IllegalAccessException e) {
-       // handle exception
-			 //System.out.println(e.toString());
-    }
+	    }
+	    catch (UnsupportedLookAndFeelException e) {
+	       // handle exception
+				 //System.out.println(e.toString());
+	    }
+	    catch (ClassNotFoundException e) {
+	       // handle exception
+				 //System.out.println(e.toString());
+	    }
+	    catch (InstantiationException e) {
+	       // handle exception
+				 //System.out.println(e.toString());
+	    }
+	    catch (IllegalAccessException e) {
+	       // handle exception
+				 //System.out.println(e.toString());
+	    }
+	
+			startGUI();
+			guiManager();
+		}
 
-		startGUI();
-		guiManager();
-	}
-
+	
+	
+	
+	
 	public static void startGUI(){
 		frame = new JFrame();
 		frame.setSize(WIDTH, HEIGHT);
@@ -80,38 +91,44 @@ public class GUI {
 	}
 
 
+	
+	
+	
+	
 	//@SuppressWarnings({"rawtypes","unchecked"})
 	public static void guiManager(){
-
-
-
+		
 		ttp = new TimeTablePanel();
 		ttp.setTimeTable(timeTable);
 		JScrollPane scrollPane = new JScrollPane(ttp);
 		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scrollPane.getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
 
-		//resoning behind minamum width and height
-		//100px for the tims on the side * 120xp width for each day.
-		//40px on top for day of the week * 20 px down for each 30 minuts (this may be to small)
+		//reasoning behind minimum width and height
+		//100px for the times on the side * 120px width for each day.
+		//40px on top for day of the week * 20px down for each 30 minutes (this may be to small)
 		ttp.setPreferredSize(new Dimension(940, 540));
 
 		JPanel options = new JPanel();
 		JScrollPane optionsSP = new JScrollPane(options);
 		optionsSP.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    optionsSP.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		optionsSP.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		optionsSP.getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
 		options.setPreferredSize(new Dimension(250, 540));
 
 		options.setLayout(new BoxLayout(options, BoxLayout.Y_AXIS));
 
+		//handles load file button
 		JButton btnLoadFile = new JButton("Load File");
 		btnLoadFile.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
-
 				File inFile = null;
 				JFileChooser fileChooser = new JFileChooser();
+				
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV FILES", "csv", "csv");
+				fileChooser.setFileFilter(filter);
+				
 				fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
 				int result = fileChooser.showOpenDialog(fileChooser);
 
@@ -122,41 +139,60 @@ public class GUI {
 				    	dr.loadData();
 				    	dr.makeCourseArray();
 				    	courses = dr.getCourseArrayList();
+				    	
+				    	DefaultListModel<String> model = new DefaultListModel<String>();
+				    	for(int i = 0; i < courses.size(); i++)
+				    		model.addElement(courses.get(i).getCouseID() + " - " + courses.get(i).getComponetID());
+				    	
+				    	courseList.setModel(model);
+				    	
 					} catch (Exception e) {
 						System.out.println("There is a problem with the file.");
 						e.printStackTrace();
 					}
 				}
-				////////////////////////////////////////////////////////
-				//prints all courses in the arraylist courses to a file called output.txt
-				/*try {
-					FileWriter out = new FileWriter(new File("output.txt"));
-					for(int i = 0; i < courses.size(); i++){
-						out.write(courses.get(i).toString());
-						out.write("\n");
-					}
-					out.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}*/
-				////////////////////////////////////////////////////////
 			}
 		});
+		
 		JPanel loadFile = new JPanel(new BorderLayout());
 		loadFile.add(btnLoadFile,BorderLayout.CENTER);
 		loadFile.setMaximumSize(new Dimension(250, 30));
 
-		//Make Time Table buttnon
+		
+		
+		
+		
+		
+		
+		//Make Time Table button
 		JButton btnMakeTimeTable = new JButton("Display Selected Courses");
 		btnMakeTimeTable.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
+				
+				ArrayList<String> selectedCourses = new ArrayList<String>();
+				
+				for(int i = 0; i < timeTableListModel.getSize(); i++)
+					selectedCourses.add(timeTableListModel.getElementAt(i));
+				
+				//ArrayList<Course> c = cm.toCourseArrayList(selectedCourses);
+				
+				
 				timeTable = new TimeTable("Test Time Table", courses);
 				timeTable.prepareCourseDrawInfo();
-				//System.out.println("Making Time Table");
 				ttp.setTimeTable(timeTable);
 				ttp.repaint();
 			}
-			});
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		JPanel makeTimeTable = new JPanel(new BorderLayout());
 		makeTimeTable.add(btnMakeTimeTable,BorderLayout.CENTER);
 		makeTimeTable.setMaximumSize(new Dimension(250, 30));
@@ -167,26 +203,65 @@ public class GUI {
 		search.add(lbSearch, BorderLayout.WEST);
 		search.add(tfSearch, BorderLayout.EAST);
 		search.setMaximumSize(new Dimension(250, 30));
-
-		String[] test= {"test1", "test1as", "tesdas1", "tescgt1", "tesgdfgt1", "test1jh", "tesjgfhht1",
-		"tesasdast1", "testdfs1", "tdsfsdst1", "tedsfst1", "teszdsfdgfhdgfhgfdghfdghdgfghfdghgfhgfderetrxcvc1", "thjgfest1", "teszcvbt1", "test11"};
-		JList<String> courseList = new JList<String>(test);
+		
+		courseList = new JList<String>(arrCourses);
 		JScrollPane courseListSP = new JScrollPane();
 		courseListSP.getViewport().add(courseList);
-    courseListSP.setPreferredSize(new Dimension(250, 200));
+		courseListSP.setPreferredSize(new Dimension(250, 200));
 
+		
+		
+		
+		
+		
+		
+		
 		JButton btnAdd = new JButton("Add Course");
+		btnAdd.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				timeTableListModel.addElement(courseList.getSelectedValue());
+				timeTableList.setModel(timeTableListModel);
+			}
+		});
+		
 		JButton btnDel = new JButton("Del Course");
+		btnDel.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				timeTableListModel.removeElement(timeTableList.getSelectedValue());
+				//timeTableListModel.addElement(courseList.getSelectedValue());
+				timeTableList.setModel(timeTableListModel);
+			}
+		});
+		
+		
+		
 		JPanel editP = new JPanel(new BorderLayout());
 		editP.add(btnAdd, BorderLayout.NORTH);
 		editP.add(btnDel, BorderLayout.CENTER);
 		editP.setMaximumSize(new Dimension(250, 60));
 
-		JList<String> timeTableList = new JList<String>(test);
+		String[] emptyDisplay = {""};
+		timeTableList = new JList<String>(emptyDisplay);
 		JScrollPane timeTableListSP = new JScrollPane();
 		timeTableListSP.getViewport().add(timeTableList);
-    timeTableListSP.setPreferredSize(new Dimension(250, 200));
+		timeTableListSP.setPreferredSize(new Dimension(250, 200));
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		options.add(loadFile);
 		options.add(search);
 		options.add(courseListSP);
